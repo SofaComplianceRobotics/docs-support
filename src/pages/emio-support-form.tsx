bdbdf8eb8853f936ec11c37b0c96b79faab96c7c
HanaeRateau <hanaerateau@yahoo.fr>
2025-03-06 19:52:26 +0100

    Replaces form by notion form iframe to handle files

--- src/pages/emio-support-form.tsx
+++ src/pages/emio-support-form.tsx
@@ -6,83 +6,6 @@ import Heading from '@theme/Heading';
 import {FormInput, FormInputProps} from '@site/src/components/FormInput/FormInput';
 import styles from './index.module.css';
 
-const fields: FormInputProps[] = [
-    {
-        label: 'Name',
-        id: 'name',
-        name: 'name',
-        type: 'text',
-        value: '',
-        onChange: () => {},
-        required: true
-    },
-    {
-        label: 'Email',
-        id: 'email',
-        name: 'email',
-        type: 'email',
-        value: '',
-        onChange: () => {},
-        required: true
-    },
-    {
-        label: 'Issue type',
-        id: 'type',
-        name: 'type',
-        type: 'select',
-        value: '',
-        onChange: () => {},
-        required: true,
-        options: ['Software', 'Hardware', 'Other']
-    },
-    {
-        label: 'Company',
-        id: 'company',
-        name: 'company',
-        type: 'text',
-        value: '',
-        onChange: () => {},
-        required: true
-    },
-    {
-        label: 'Serial Number',
-        id: 'serialnumber',
-        name: 'serialnumber',
-        type: 'text',
-        value: '',
-        onChange: () => {},
-        required: true
-    },
-    {
-        label: 'Issue Title',
-        id: 'issue',
-        name: 'issue',
-        type: 'text',
-        value: '',
-        onChange: () => {},
-        required: true
-    },
-    {
-        label: 'Description',
-        id: 'description',
-        name: 'description',
-        type: 'textarea',
-        value: '',
-        onChange: () => {},
-        required: true
-    },
-    {
-        label: 'Files',
-        id: 'files',
-        name: 'files',
-        type: 'file',
-        value: '',
-        onChange: () => {},
-        required: true,
-        multipleFiles: true
-    }
-];
-
 function PageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
@@ -123,6 +46,13 @@ const EmioSupportForm: React.FC = () => {
         e.preventDefault();
         // Handle form submission logic here
         console.log('Form submitted:', formData);
+
+        fetch("https://hook.eu2.make.com/p8xp4r3e4rd7jfh938yi33vzk5bb3e21", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"},
+            body: JSON.stringify(formData)
+        })
     };
 
     function handleTypeSelected(event: React.ChangeEvent<HTMLSelectElement>): void {
@@ -141,12 +71,88 @@ const EmioSupportForm: React.FC = () => {
         });
     };
 
+    const fields: FormInputProps[] = [
+        {
+            label: 'Name',
+            id: 'name',
+            name: 'name',
+            type: 'text',
+            value: '',
+            onChange: handleChange,
+            required: true
+        },
+        {
+            label: 'Email',
+            id: 'email',
+            name: 'email',
+            type: 'email',
+            value: '',
+            onChange: handleChange,
+            required: true
+        },
+        {
+            label: 'Issue type',
+            id: 'type',
+            name: 'type',
+            type: 'select',
+            value: '',
+            onChange: handleTypeSelected,
+            required: true,
+            options: ['Software', 'Hardware', 'Other']
+        },
+        {
+            label: 'Company',
+            id: 'company',
+            name: 'company',
+            type: 'text',
+            value: '',
+            onChange: handleChange,
+            required: true
+        },
+        {
+            label: 'Serial Number',
+            id: 'serialnumber',
+            name: 'serialnumber',
+            type: 'text',
+            value: '',
+            onChange: handleChange,
+            required: true
+        },
+        {
+            label: 'Issue Title',
+            id: 'issue',
+            name: 'issue',
+            type: 'text',
+            value: '',
+            onChange: handleChange,
+            required: true
+        },
+        {
+            label: 'Description',
+            id: 'description',
+            name: 'description',
+            type: 'textarea',
+            value: '',
+            onChange: handleChange,
+            required: true
+        },
+        {
+            label: 'Files',
+            id: 'files',
+            name: 'files',
+            type: 'file',
+            onChange: handleFilesChange,
+            required: false,
+            multipleFiles: true
+        }
+    ];
+
     return (
         <Layout>
             <PageHeader />
             <div className="container" style={{ padding: '2rem' }}>
             <div className='row' style={{ justifyContent: 'center' }}>
-            <form onSubmit={handleSubmit}>
+            {/* <form onSubmit={handleSubmit}>
                 {fields.map((field, index) => (
                     <FormInput
                         key={index}
@@ -155,7 +161,7 @@ const EmioSupportForm: React.FC = () => {
                         name={field.name}
                         type={field.type}
                         value={formData[field.name]}
-                        onChange={handleChange}
+                        onChange={field.onChange}
                         required={field.required}
                         options={field.options}
                         multipleFiles={field.multipleFiles}
@@ -163,7 +169,8 @@ const EmioSupportForm: React.FC = () => {
                 ))    
                 }
                 <button type="submit">Submit</button>
-            </form>
+            </form> */}
+            <iframe src="https://compliance-robotics.notion.site/ebd/135582ce7d3a8042a130ceab6cca0d39" width="100%" height="2000vh" />
             </div>
         </div>
 
