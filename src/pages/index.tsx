bfa428e38baa5a546367feda1e4d2e218f44d034
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-03-15 16:41:09 +0100

    improves homepage banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,26 +11,27 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          Compliance Robotics | 
-          Docs & Support
-        </Heading>
-        <p className="hero__subtitle">Explore our documentation and get the help you need.</p>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx("button button--secondary button--lg", styles.whitebutton)}
-            to="/docs">
-            Get Started
-          </Link>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div style={{display: "flex", alignItems:"center"}}>
+          <div className="container" style={{flex: "1 1 0%"}}>
+            <Heading as="h1" className="hero__title">
+              Compliance Robotics | 
+              Docs & Support
+            </Heading>
+            <p className="hero__subtitle">Explore our documentation and get the help you need.</p>
+            <br/>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx("button button--secondary button--lg", styles.whitebutton)}
+                to="/docs">
+                Get Started
+              </Link>
+            </div>
+          </div>
+          <img src="img/emio_SOFA.jpg" style={{width:"40%", height:"100%", borderRadius:"0", clipPath: "polygon(20% 0, 100% 0, 100% 100%, 0% 100%)"
+}}/>
         </div>
-        <br/>
-        <div className='center'>
-           <img src="img/emio_SOFA.jpg" width="30%"/>
-        </div>
-      </div>
-    </header>
+      </header>
   );
 }
 
