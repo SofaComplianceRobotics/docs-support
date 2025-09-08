f82e708dfad43b7119ebbac2a4e5c4f9e8e407de
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-09-08 15:54:06 +0200

    Update src/pages/index.tsx
    Co-authored-by: HanaeRateau <hanaerateau@yahoo.fr>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ function HomepageHeader() {
             <div className={styles.buttons}>
               <Link
                 className={clsx("button button--secondary button--lg", styles.whitebutton)}
-                to={`${preferredVersion.preferredVersion?preferredVersion.preferredVersion.path:"https://docs-support.compliance-robotics.com/docs/next/"}`}>
+                to={`${preferredVersion.preferredVersion?preferredVersion.preferredVersion.path:"docs/next"}`}>
                 Get Started
               </Link>
             </div>
